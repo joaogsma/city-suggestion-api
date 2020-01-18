4d38d713b538829c0d4112eeea536900bb4f1b15
João Gabriel Abreu <joao.abreu@inlocomedia.com>
2020-01-18 01:20:49 -0500

    feat(core/actions): add the FindCandidateCitiesAction with tests

--- core/src/main/java/org/joaogsma/citysuggestion/core/actions/FindCandidateCitiesAction.java
+++ core/src/main/java/org/joaogsma/citysuggestion/core/actions/FindCandidateCitiesAction.java
@@ -1,14 +1,24 @@
 package org.joaogsma.citysuggestion.core.actions;
 
+import java.util.Set;
 import java.util.stream.Stream;
 import javax.inject.Inject;
+import org.joaogsma.citysuggestion.core.dal.InvertedIndexFacade;
 import org.joaogsma.citysuggestion.core.models.City;
+import org.joaogsma.citysuggestion.core.text.ExtractTrigramsFunction;
 
 public class FindCandidateCitiesAction {
+  private final InvertedIndexFacade facade;
+  private final ExtractTrigramsFunction extractTrigramsFn;
+
   @Inject
-  FindCandidateCitiesAction() {}
+  FindCandidateCitiesAction(InvertedIndexFacade facade, ExtractTrigramsFunction extractTrigramsFn) {
+    this.facade = facade;
+    this.extractTrigramsFn = extractTrigramsFn;
+  }
 
   public Stream<City> call(String searchTerm) {
-    return null;
+    final Set<String> trigrams = extractTrigramsFn.apply(searchTerm);
+    return facade.get(trigrams.stream());
   }
 }
